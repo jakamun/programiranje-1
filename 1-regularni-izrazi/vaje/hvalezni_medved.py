d6e696e5b4cead9caf196994e6cbebce3f0e4bbf
Jaka Munda <jaka.munda@siol.net>
2018-10-19 19:55:44 +0200

    prva vaja

--- 1-regularni-izrazi/vaje/hvalezni_medved.py
+++ 1-regularni-izrazi/vaje/hvalezni_medved.py
@@ -27,12 +27,14 @@ medved. Zvrhano zibelko sladkih hrušk mi je prinesel za en sam izdrt trn"."""
 ###############################################################################
 def find_words(besedilo, niz):
     import re
-    seznam = set()
-    regularni =  r'\w*' + niz + r'\w*'
-    for beseda in re.findall(regularni, besedilo):
-        seznam.add(beseda)
-    return seznam
+    slovar = set()
+    regularni =  r'\b\w*' + niz + r'\w*\b'
+    seznam = re.findall(regularni, besedilo)
+    for beseda in seznam:
+        slovar.add(beseda)
+    return slovar
 
+ujemajoce_besede = find_words(test_text, 'de')
 
 ###############################################################################
 # 2) Sestavite funkcijo [find_prefix], ki vrne množico vseh besed, ki se
@@ -41,7 +43,16 @@ def find_words(besedilo, niz):
 # >>> find_prefix(test_text, 'zi')
 # {'zibala', 'zibel', 'zibelko'}
 ###############################################################################
+def find_prefix(besedilo, predpona):
+    import re
+    slovar = set()
+    regularni = r'(\b' + predpona + r'\w*\b)'
+    seznam_ujemanj = re.findall(regularni, besedilo)
+    for beseda in seznam_ujemanj:
+        slovar.add(beseda)
+    return slovar
 
+besede_z_enako_predpono = find_prefix(test_text, 'zi')
 
 ###############################################################################
 # 3) Sestavite funkcijo [find_suffix], ki vrne množico vseh besed, ki se
@@ -50,7 +61,15 @@ def find_words(besedilo, niz):
 # >>> find_suffix(test_text, 'la')
 # {'zibala', 'razveselila', 'prestrašila', 'šivala', 'opazila', 'tla'}
 ###############################################################################
+def find_suffix(besedilo, pripona):
+    import re
+    slovar = set()
+    regularni = r'\b\w*?' + pripona + r'\b'
+    for beseda in re.findall(regularni, besedilo):
+        slovar.add(beseda)
+    return slovar
 
+besede_z_enako_pripono = find_suffix(test_text, 'la')
 
 ###############################################################################
 # 4) Sestavite funkcijo [double_letters], ki sprejme niz in vrne množico vseh
@@ -59,3 +78,13 @@ def find_words(besedilo, niz):
 # >>> double_letters('A volunteer is worth twenty pressed men.')
 # {'volunteer', 'pressed'}
 ###############################################################################
+def double_letters(niz):
+    '''Funkcija mi vrne mnozico naborov v katerih je na prvem mestu beseda, na drugem pa črka ki se podvoji'''
+    import re
+    slovar = set()
+    regularni = r'(\b\w*?(?P<dvojno>\w)(?P=dvojno)+\w*\b)'
+    for beseda in re.findall(regularni, niz):
+        slovar.add(beseda)
+    return slovar
+
+podvojene_crke = double_letters('A volunteer is worth twenty pressed men.')
